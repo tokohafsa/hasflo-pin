9980a884094a914f56ebe3bd37d24a12a04e811d
tokohafsa <tokohafsa@gmail.com>
2026-07-19 19:14:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # ============================================================
 
 # --- AI Settings (opsional) ---
-AI_ENABLED = True  # Ganti True kalau mau pakai AI
+
 AI_MODEL = "gemini-3.1-flash-lite"  # Model untuk text (title, description, jadwal)
 AI_IMAGE_MODEL = (
     "gemini-3.1-flash-image"  # Model untuk image generation (Nano Banana 2)
